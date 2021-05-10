1e8c72bea3773d10aea19f1a6a7bd93ac1cb0ada
Path-may1 <80159654+Path-may1@users.noreply.github.com>
2021-05-10 23:19:20 +0000

    new updates

--- express_server.js
+++ express_server.js
@@ -58,3 +58,7 @@ app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
+
+function generateRandomString() {
+
+}
