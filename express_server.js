fe8baff14ae1b00f21f1f91056f5e422e2eb2a14
Path-may1 <80159654+Path-may1@users.noreply.github.com>
2021-05-10 20:11:10 +0000

    updated changes

--- express_server.js
+++ express_server.js
@@ -18,3 +18,15 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+app.get("/hello", (req, res) => {
+  res.send("<html><body>hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
