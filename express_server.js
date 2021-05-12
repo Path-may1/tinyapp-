ab40af285c9dd4d70af3b4e64a849f7e9dcc49b5
Path-may1 <80159654+Path-may1@users.noreply.github.com>
2021-05-12 17:00:33 +0000

    modified login/logout

--- express_server.js
+++ express_server.js
@@ -1,26 +1,40 @@
 const express = require("express");
+
+// creating an Express app
 const app = express();
+
 const PORT = 8080; // default port 8080
 
+//settign ejs as template engine
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
 //app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 let cookieParser = require('cookie-parser')
-app.use(cookieParser())
+app.use(cookieParser());
 
 // const templateVars = {
 //  username : cookieName,
 //   //username: req.cookies["username"]
 //   // ... any other vars
 // };
-res.render("urls_index", templateVars);
 
+app.get("/urls", (req, res) => {
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"]
+  };
+  res.render("urls_index", templateVars);
+});
+// datebase => shortUrl and longUrl
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "4mp89x": "http://amazon.ca"
 };
+
+// end points || routes
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -37,34 +51,34 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>hello <b>World</b></body></html>\n");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// });
 
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
-app.get("/my_urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
+
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
 app.get("/u/:shortURL", (req, res) => {
- console.log("received request")
-  const shortURL = req.params.shortURL 
-   const longURL = urlDatabase[shortURL]
+  console.log("received request")
+  const shortURL = req.params.shortURL
+  const longURL = urlDatabase[shortURL]
   res.redirect(longURL);
 });
 
@@ -73,10 +87,14 @@ app.get("/urls/:shortURL", (req, res) => {
   console.log(shortURL)
   console.log("edit")
   console.log("tinyapp")
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL] };
+  const templateVars = {
+     shortURL: req.params.shortURL,
+      longURL: urlDatabase[shortURL],
+      username: req.cookies["username"]
+      };
   res.render("urls_show", templateVars);
 });
- 
+
 
 app.post("/urls", (req, res) => {
   console.log(req.body.longURL);  // Log the POST request body to the console
@@ -85,7 +103,7 @@ app.post("/urls", (req, res) => {
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 // new code added for delete
-app.post('/urls/:shortURL/delete',  (req,res) => {
+app.post('/urls/:shortURL/delete', (req, res) => {
   console.log(req.params)
   // extract the id from the path :req.params.shortUrl
   const id = req.params.shortURL
@@ -94,39 +112,40 @@ app.post('/urls/:shortURL/delete',  (req,res) => {
   //redirect to tinyap
   res.redirect('/urls');
 });
-
-app.post('/urls/edit/:shortURL', (req,res) => {
+//add 
+app.post('/urls/edit/:shortURL', (req, res) => {
   console.log(req.params.shortURL)
-const id = req.params.shortURL
+  const id = req.params.shortURL
   console.log(req.body)
-urlDatabase[id] = req.body.Url
-res.redirect('/urls')
+  urlDatabase[id] = req.body.Url
+  res.redirect('/urls')
 })
-
-app.post('/login', (req,res) => {
+// Login route
+app.post('/login', (req, res) => {
   const cookieName = req.body.username
-  res.cookie("username",cookieName) 
+  res.cookie("username", cookieName)
   console.log(cookieName)
   res.redirect('/urls')
 })
-
-// app.post('/logout', (req,res) => {
-//   const cookieName = req.body.username
-//   res.cookie("username",cookieName) 
-//   console.log(cookieName)
-//   res.redirect('/urls')
-// })
+// Logout route
+app.post('/logout', (req, res) => {
+  //   const cookieName = req.body.username
+  //   res.cookie("username",cookieName) 
+  //   console.log(cookieName)
+  res.clearCookie('username', req.body.username )
+     res.redirect('/urls')
+})
 
 function generateRandomString() {
   let result1 = "";
   let result = [];
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let charactersLength = characters.length;
-  
+
   for (let i = 0; i < 6; i++) {
     result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
   }
 
-  return  result.join('');
-  
+  return result.join('');
+
 }
